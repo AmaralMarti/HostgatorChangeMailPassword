18a7fe7869dd102073f27c226a14fb1c9465279f
Henrique Marti <henrique@webmarti.com.br>
2016-09-16 13:42:31 -0300

    Acertei a lista de domínios retornada

--- getdomains.py
+++ getdomains.py
@@ -11,15 +11,15 @@ def get_domains():
 
     for dir in os.listdir(etc_path):
     	if os.path.isdir(dir):
-        	domains.append('@' + dir)
+        	domains.append(dir)
 
     return domains
 
 
 if __name__ == "__main__":
     print "Content-Type: text/html\n\n"
-    
+
     domains = get_domains()
 
     for domain in domains:
-        print "<option value='" + domain + "'>" + domain + "</option>"
+        print domain
